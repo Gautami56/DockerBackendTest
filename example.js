5f52f82c16570797945447fade562f1100f0ba94
Gautami56 <gautu.pinkyar@gmail.com>
2024-04-21 14:10:34 +0530

    implement simple node app

--- example.js
+++ example.js
@@ -1,11 +1,13 @@
-var express = require("express");
+// var express = require("express");
 var moment = require("moment");
-var app = express();
+var date = moment().format("LL");
+console.log("Date is :", date);
+// var app = express();
 
-app.get("/home", (req, res) => {
-  var date = moment().format("LL");
-  console.log("Date is :", date);
-  res.send("Heyyy Heloow");
-});
+// app.get("/home", (req, res) => {
+//   var date = moment().format("LL");
+//   console.log("Date is :", date);
+//   res.send("Heyyy Heloow");
+// });
 
-app.listen(3006);
+// app.listen(3006);
